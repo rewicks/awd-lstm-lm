fec13cfb014af99038895473badbf8874cd2800c
Rachel Wicks <rachel@wicks.me>
2020-05-06 09:59:44 -0400

    corpus dictionary fix

--- data.py
+++ data.py
@@ -21,14 +21,14 @@ class Dictionary(object):
             self.counter = Counter()
             self.total = 0
 
-    def add_word(self, word):
-        if word not in self.word2idx:
+    def add_word(self, word, preexist=True):
+        if word not in self.word2idx and not preexist:
             self.idx2word.append(word)
             self.word2idx[word] = len(self.idx2word) - 1
-        token_id = self.word2idx[word]
+        token_id = self.word2idx.get(word, self.word2idx['<unk>'])
         self.counter[token_id] += 1
         self.total += 1
-        return self.word2idx[word]
+        return token_id
 
     def __len__(self):
         return len(self.idx2word)
@@ -37,11 +37,15 @@ class Dictionary(object):
 class Corpus(object):
     def __init__(self, path, vocab_file=None):
         self.dictionary = Dictionary(vocab_file)
-        self.train = self.tokenize(os.path.join(path, 'train.txt'))
-        self.valid = self.tokenize(os.path.join(path, 'valid.txt'))
-        self.test = self.tokenize(os.path.join(path, 'test.txt'))
+        if vocab_file is not None:
+            preexist=True
+        else:
+            preexist=False
+        self.train = self.tokenize(os.path.join(path, 'train.txt'), preexist=preexist)
+        self.valid = self.tokenize(os.path.join(path, 'valid.txt'), preexist=preexist)
+        self.test = self.tokenize(os.path.join(path, 'test.txt'), preexist=preexist)
 
-    def tokenize(self, path):
+    def tokenize(self, path, preexist=True):
         """Tokenizes a text file."""
         assert os.path.exists(path)
         # Add words to the dictionary
@@ -51,7 +55,7 @@ class Corpus(object):
                 words = line.split() + ['</s>']
                 tokens += len(words)
                 for word in words:
-                    self.dictionary.add_word(word)
+                    self.dictionary.add_word(word, preexist=preexist)
 
         # Tokenize file content
         with open(path, 'r') as f:
